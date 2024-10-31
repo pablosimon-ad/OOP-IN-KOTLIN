a6889d18dd0cded3aaf498cbd2cb68fbedbe7da7
pablosimon-ad <psi549924@iesvirreymorcillo.es>
2024-10-31 12:59:10 +0100

    commit. Repaso de Kotlin y Programacion orientada a objetos.

--- src/chapter4/Dog.kt
+++ src/chapter4/Dog.kt
@@ -1,4 +1,24 @@
 package chapter4
 
-class Dog {
+import java.util.*
+
+class Dog (val name: String, weightParam: Int, breedParam: String){
+    init{
+        print("Dog $name has been created.")
+    }
+    var activities = arrayOf("Walks")
+    val breed = breedParam.uppercase(Locale.getDefault())
+    init{
+        println("The breed is $breed.")
+    }
+    var weight = weightParam
+        set(value){
+            if(value>0) field=value
+        }
+    val weightIntKgs: Double
+        get() = weight / 2.2
+
+    fun bark(){
+        println(if(weight<20)"woof!" else "WOOF!")
+    }
 }
--- src/chapter4/Main.kt
+++ src/chapter4/Main.kt
@@ -1,14 +1,24 @@
+package chapter4
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(args: Array<String>) {
+val myDog = Dog("Fido",70,"Mixed")
+    myDog.bark()
+    myDog.weight = 75
+    println("Weight in kgs is ${myDog.weightIntKgs}")
+    myDog.weight = -2
+    println("Weight is ${myDog.weight}")
+    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+    for (item in myDog.activities){
+        println("My dog enjoys $item")
     }
+    val dogs = arrayOf(
+        Dog("Kelpie", 20, "Westie"),
+        Dog("Charlie", 35, "Labrador")
+    )
+    dogs[0].bark()
+    dogs[0].weight = 15
+    println("Weight for ${dogs[0].name} is ${dogs[0].weight}")
+
 }
